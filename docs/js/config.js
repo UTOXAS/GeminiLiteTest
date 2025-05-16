a8c24beacc17f707ef57ccd056077055d08941af
Abdullah Saleh <utoxas@outlook.com>
2025-05-16 22:44:53 +0300

    Update Apps Script URL in config.js for deployment AKfycbw8i6rQDfxkC3piD8LNS8hBFeJphLtfRMvxfEnPFH1OxxyGZvl95tUc8vroW2weNdif

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,1 +1,1 @@
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxuZLCkCVb2yROhT5p6tsuKLxo0bEe58Qik7cAKpOUeKD251LY4oRSSUkzzBwKbmZqV/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw8i6rQDfxkC3piD8LNS8hBFeJphLtfRMvxfEnPFH1OxxyGZvl95tUc8vroW2weNdif/exec';
